ec287c90cb3e99391b097ab9f8646ec9850d33af
yuta.onishi <rad6242@icloud.com>
2024-07-13 21:37:48 +0900

    testしたいのは構造体のフィールドだった

--- internal/timer/timer_test.go
+++ internal/timer/timer_test.go
@@ -9,7 +9,7 @@ func TestTimer(t *testing.T) {
 	duration := 3 * time.Second
 	timer := NewTimer(duration)
 
-	if timer.Duration() != duration {
+	if timer.duration != duration {
 		t.Errorf("Expected duration to be %v, got %v", duration, timer.duration)
 	}
 }
