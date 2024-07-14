ec2f745115f176cf72f0f6e831981d9345dd0152
yuta.onishi <rad6242@icloud.com>
2024-07-14 15:14:09 +0900

    session関連のテストと実装

--- internal/pomodoro/pomodoro.go
+++ internal/pomodoro/pomodoro.go
@@ -25,3 +25,26 @@ func NewPomodoro(workDuration, shortBreak, longBreak time.Duration, sessions int
 		currentSession: 0,
 	}
 }
+
+func (p *Pomodoro) Start() {
+	p.currentSession++
+
+	if p.currentSession <= p.sessions {
+		p.timer = timer.NewTimer(p.workDuration)
+	} else {
+		p.timer = timer.NewTimer(p.longBreak)
+		p.currentSession = 0
+	}
+	p.timer.Start()
+}
+
+func (p *Pomodoro) NextSession() {
+	if p.currentSession == p.sessions {
+		p.timer = timer.NewTimer(p.longBreak)
+		p.currentSession = 0
+	} else {
+		p.timer = timer.NewTimer(p.shortBreak)
+		p.currentSession++
+	}
+	p.timer.Start()
+}
--- internal/pomodoro/pomodoro_test.go
+++ internal/pomodoro/pomodoro_test.go
@@ -25,3 +25,73 @@ func TestNewPomodoro(t *testing.T) {
 		t.Errorf("Expected session to be %v, got %v", session, pomodoro.sessions)
 	}
 }
+
+func TestPomodoro_Start_FirstSession(t *testing.T) {
+	workDuration := 5 * time.Millisecond
+	shortBreak := 2 * time.Millisecond
+	longBreak := 3 * time.Millisecond
+	session := 2
+	pomodoro := NewPomodoro(workDuration, shortBreak, longBreak, session)
+
+	startTime := time.Now()
+	pomodoro.Start()
+
+	if pomodoro.currentSession != 1 {
+		t.Errorf("Expected current session to be 1, got %v", pomodoro.currentSession)
+	}
+
+	<-pomodoro.timer.Done()
+	elapsedTime := time.Since(startTime)
+
+	if elapsedTime < workDuration {
+		t.Errorf("Expected elapsed time to be at least %v, got %v", workDuration, elapsedTime)
+	}
+}
+
+func TestPomodoro_NextSession_LastSession(t *testing.T) {
+	workDuration := 5 * time.Millisecond
+	shortBreak := 2 * time.Millisecond
+	longBreak := 3 * time.Millisecond
+	sessions := 2
+	pomodoro := NewPomodoro(workDuration, shortBreak, longBreak, sessions)
+
+	pomodoro.currentSession = sessions
+
+	startTime := time.Now()
+	pomodoro.NextSession()
+
+	<-pomodoro.timer.Done()
+	elapsedTime := time.Since(startTime)
+
+	if elapsedTime < longBreak {
+		t.Errorf("Expected elapsed time to be at least %v, got %v", longBreak, elapsedTime)
+	}
+
+	if pomodoro.currentSession != 0 {
+		t.Errorf("Expected current session to be 0, got %d", pomodoro.currentSession)
+	}
+}
+
+func TestPomodoro_NextSession_NotLastSession(t *testing.T) {
+	workDuration := 5 * time.Millisecond
+	shortBreak := 2 * time.Millisecond
+	longBreak := 3 * time.Millisecond
+	sessions := 2
+	pomodoro := NewPomodoro(workDuration, shortBreak, longBreak, sessions)
+
+	pomodoro.currentSession = sessions - 1
+
+	startTime := time.Now()
+	pomodoro.NextSession()
+
+	<-pomodoro.timer.Done()
+	elapsedTime := time.Since(startTime)
+
+	if elapsedTime < shortBreak {
+		t.Errorf("Expected elapsed time to be at least %v, got %v", shortBreak, elapsedTime)
+	}
+
+	if pomodoro.currentSession != sessions {
+		t.Errorf("Expected current session to be %d, got %d", sessions, pomodoro.currentSession)
+	}
+}
